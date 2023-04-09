2bc557c36c7ae69cea230cfd5491b224dac98cc1
Ömer Çetin <94043864+omercetinn@users.noreply.github.com>
2023-04-09 13:55:35 +0300

    Tracker and intercaptor

--- Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
+++ Infrastructure/ETicaretAPI.Persistence/ServiceRegistration.cs
@@ -9,6 +9,8 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Persistence.Repositories;
 
 namespace ETicaretAPI.Persistence
 {
@@ -17,7 +19,14 @@ namespace ETicaretAPI.Persistence
         public static void AddPersistenceServices(this IServiceCollection services)
         {
             services.AddSingleton<IProductService,ProductService>();
-            services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
+            services.AddDbContext<ETicaretAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString),ServiceLifetime.Singleton);
+
+            services.AddScoped<IProductReadRepository, ProductReadRepository>();
+            services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
+            services.AddScoped<ICustomerReadRepository, CustomerReadRepository>();
+            services.AddScoped<ICustomerWriteRepository, CustomerWriteRepository>();
+            services.AddScoped<IOrderReadRepository, OrderReadRepository>();
+            services.AddScoped<IOrderWriteRepository, OrderWriteRepository>();
         }
     }
 }
--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -1,4 +1,7 @@
 ﻿using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities;
+using ETicaretAPI.Persistence.Repositories;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,16 +12,78 @@ namespace ETicaretAPI.API.Controllers
     public class ProductsController : ControllerBase
     {
         private readonly IProductService _productService;
+        private readonly IProductReadRepository _productReadRepository;
 
-        public ProductsController(IProductService productService)
+        private readonly IProductWriteRepository _productWriteRepository;
+
+        readonly private IOrderWriteRepository _orderWriteRepository;
+
+        readonly private IOrderReadRepository _orderReadRepository;
+
+        readonly private ICustomerWriteRepository _customerWriteRepository;
+
+
+        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository, IProductService productService, IOrderWriteRepository orderWriteRepository, ICustomerWriteRepository customerWriteRepository, IOrderReadRepository orderReadRepository)
         {
+            _productWriteRepository = productWriteRepository;
             _productService = productService;
+            _productReadRepository = productReadRepository;
+            _orderWriteRepository = orderWriteRepository;
+            _customerWriteRepository = customerWriteRepository;
+            _orderReadRepository = orderReadRepository;
         }
+
         [HttpGet]
         public IActionResult Get()
         {
             var products = _productService.GetProducts();
             return Ok(products);
         }
+
+        //[HttpGet]
+        //public async Task Get1()
+        //{
+        //    //await _productWriteRepository.AddRangeAsync(new()
+        //    //{
+        //    //    new() {Id = Guid.NewGuid(),Name="Pro1",Price=500,CreatedDate=DateTime.UtcNow,Stock=5},
+        //    //    new() {Id = Guid.NewGuid(),Name="Pro2",Price=400,CreatedDate=DateTime.UtcNow,Stock=8},
+        //    //    new() {Id = Guid.NewGuid(),Name="Pro3",Price=300,CreatedDate=DateTime.UtcNow,Stock=9},
+        //    //});
+
+        //    //var count = await _productWriteRepository.SaveAsync();
+        //    Product product = await _productReadRepository.GetByIdAsync("7a1cd13b-efc2-4d61-a0ea-1275230732d8",false);
+        //    product.Name = "Mehmet";
+        //    await _productWriteRepository.SaveAsync();          
+        //}
+
+        //[HttpPost]
+        //public async Task Get2()
+        //{
+        //    var customerId=Guid.NewGuid();
+        //    await _customerWriteRepository.AddAsync(new Customer { Id = customerId, Name = "Muhittin" });
+
+        //    await _orderWriteRepository.AddRangeAsync(new()
+        //    {
+        //        new() {Description="bla bla",Address="Ankara,Çankaya",CustomerId=customerId},
+        //        new() {Description="bla bla",Address="Ankara,Pursaklar",CustomerId = customerId}
+        //    });
+
+        //   await _orderWriteRepository.SaveAsync();
+        //}
+        [HttpPost]
+        public async Task Get3()
+        {
+            Order order = await _orderReadRepository.GetByIdAsync("14c90c26-5f3b-442d-8c8e-7f2812bd355f");
+            order.Address = "Istanbul";
+            await _orderWriteRepository.SaveAsync();
+        }
+
+        //[HttpGet("{id}")]
+        //public async Task<IActionResult> Get(string id)
+        //{
+        //    Product product= await _productReadRepository.GetByIdAsync(id);
+        //    return Ok(product);
+
+        //}
     }
 }
